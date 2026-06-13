15f0ed5c2e710e1e41587ced8c268b1427bb586e
Victor <victoriyari@hotmail.com>
2026-06-13 11:01:30 -0600

    Just a test commit to see if I can push to GitHub from windows terminal.

---
+++ Week1/inventory/Inventory/Classes/Garage.cs
@@ -0,0 +1,15 @@
+﻿namespace Inventory.Classes;
+
+//This class is only to fulfill inheritance requirements but its not going to be used by the program
+public class Garage : StockItems
+{
+    public Garage(int quantity, string type, string name) : base(quantity, type, name)
+    {
+        
+    }
+
+    public override string Details()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Week1/inventory/Inventory/Program.cs
@@ -0,0 +1,26 @@
+﻿using Inventory.Classes;
+
+if (true)
+{
+    test();
+}
+
+static void test()
+{
+    Console.WriteLine("Welcome to the Inventory Program!");
+    StockItems[] storage =
+    {
+        new Store(10, "Car", "Ford", "Ford", 1000, 900, true),
+        new Store(10, "Car", "Chevy", "Chevy", 1200, 1100, false),
+        new Store(10, "Car", "BMW", "BMW", 1500, 1400, false),
+        new Store(10, "Car", "Tesla", "Tesla", 2000, 1900, false),
+        new Store(10, "Car", "Audi", "Audi", 2500, 2400, false),
+        new Store(10, "Car", "Subaru", "Subaru", 3000, 2900, false),
+        new Store(10, "Car", "Kia", "Kia", 3500, 3400, false),
+    };
+
+    foreach (var item in storage)
+    {
+        Console.WriteLine(item.Details());
+    }
+}
